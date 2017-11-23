a4321f4826ef6cdbe4daa51b7e591f9691a2ef66
jerry <729083394@qq.com>
2017-11-23 20:02:59 +0800

    test

--- work/Jedis_demo/src/test/java/com/demo/jerry/Jedis_demo/RedisTest.java
+++ work/Jedis_demo/src/test/java/com/demo/jerry/Jedis_demo/RedisTest.java
@@ -1,5 +1,35 @@
 package com.demo.jerry.Jedis_demo;
 
-public class RedisTest {
+import org.junit.Test;
 
+import redis.clients.jedis.Jedis;
+
+public class RedisTest {
+	
+	/**
+	 * 测试连接
+	 */
+	@Test
+	public void testJedisDemo1() {
+		try {
+			Jedis jedis  = new Jedis("192.168.32.128",6379);
+			jedis.set("name","何锦程");
+			String name = jedis.get("name");
+			System.out.println(name);
+			jedis.flushAll();
+			jedis.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/*
+	 * 测试连接池连接
+	 * 
+	 */
+	@Test
+	public void testJedisDemo2() {
+		
+	}
 }
