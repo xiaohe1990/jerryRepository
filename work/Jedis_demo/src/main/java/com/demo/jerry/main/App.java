3a8ea5583633ade7aa3f682ad9c13ed29ddd186b
jerry <729083394@qq.com>
2017-12-14 20:56:14 +0800

    配置可运行jar包

--- work/Jedis_demo/src/main/java/com/demo/jerry/main/App.java
+++ work/Jedis_demo/src/main/java/com/demo/jerry/main/App.java
@@ -1,5 +1,7 @@
 package com.demo.jerry.main;
 
+import redis.clients.jedis.Jedis;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        try {
+			Jedis jedis  = new Jedis("192.168.32.128",6379);
+			jedis.set("name","何锦程");
+			String name = jedis.get("name");
+			System.out.println(name);
+			//jedis.flushAll();
+			jedis.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 }
